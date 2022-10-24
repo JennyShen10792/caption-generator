bb2472300725e0977ae2f7b1ecea03fbee5a8340
JennyShen10792 <js10792@nyu.edu>
2022-10-24 03:53:17 -0400

    Edit folders and add css styling

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(request, response){
-	response.sendFile('home.html', { root: '.' })
+	response.sendFile('/public/home.html', { root: '.' })
 });
 app.listen(3000);
