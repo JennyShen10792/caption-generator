9c58499d0aab935fd0c88cefa040ab548493444c
JennyShen10792 <js10792@nyu.edu>
2022-10-27 19:10:08 -0400

    Start scraping

---
+++ src/scraperapi.js
@@ -0,0 +1,29 @@
+const cheerio = require('cheerio');
+
+const puppeteer = require('puppeteer');
+
+let scraped_headlines = [];
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+    try {
+await page.goto('https://www.lyrics.com/lyrics/heart', {timeout: 180000});
+let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+let $ = cheerio.load(bodyHTML);
+let article_headlines = $('a[href*="/lyric-lf"] > div')
+article_headlines.each((index, element) => {
+title = $(element).find('pre').text()
+scraped_headlines.push({
+'title': title
+})
+});
+
+      }  catch(err) {
+          console.log(err);
+      }
+
+      await browser.close();
+    console.log(scraped_headlines)
+})();
