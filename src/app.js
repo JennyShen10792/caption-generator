1ae0e8f9a2b9b540199cd75d690418e6390ee88c
JennyShen10792 <js10792@nyu.edu>
2022-11-03 21:25:53 -0400

    Edit functions to be client side

--- src/app.js
+++ src/app.js
@@ -1,8 +1,60 @@
 const express = require('express');
 const app = express();
 
+const cheerio = require('cheerio');
+
+const puppeteer = require('puppeteer');
+
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response){
 	response.sendFile('/public/home.html', { root: '.' })
+});
+
+
+
+
+
+app.get('/api/captions', function(req, res) {
+  // TODO: retrieve all reviews or use filters coming in from req.query
+  // send back as JSON list
+	console.log(req.query.type);
+	if (req.query.type != null) {
+		console.log("abs");
+		let lyrics_list = [];
+		
+(async () => {
+console.log("hi");
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+    try {
+    console.log("hello");
+await page.goto('https://www.lyrics.com/lyrics/' + req.query.type, {timeout: 180000});
+let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+console.log(bodyHTML);
+let $ = cheerio.load(bodyHTML);
+let label = $('.lyric-meta-title'); 
+let output = label.find('pre').text();
+console.log(output);
+$('.lyric-body').each((index, element) => {
+const lyrics = $(element).text();
+lyrics_list.push({
+'lyrics': lyrics
+})
+});
+
+      }  catch(err) {
+          console.log(err);
+      }
+
+      await browser.close();
+    console.log(lyrics_list)
+    res.send(lyrics_list);	
+})();
+		
+
+	
+	
+	}
 });
 app.listen(3000);
