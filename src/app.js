78725c7ef634ad78b123a8e3a298e0db1f1d7822
JennyShen10792 <js10792@nyu.edu>
2022-11-05 20:01:20 -0400

    Finish connecting lyrics to print on screen

--- src/app.js
+++ src/app.js
@@ -16,22 +16,22 @@ app.get('/api/captions', function(req, res) {
 
 	console.log(req.query.type);
 	if (req.query.type != null) {
-	//	console.log("abs");
+	
 		let lyrics_list = [];
 		
 		(async () => {
-	//	console.log("hi");
+	
 		  const browser = await puppeteer.launch();
 		  const page = await browser.newPage();
    		  try {
-   		//	console.log("hello");
+   		
 			await page.goto('https://www.lyrics.com/lyrics/' + req.query.type, {timeout: 180000});
 			let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-			console.log(bodyHTML);
+			
 			let $ = cheerio.load(bodyHTML);
 			let label = $('.lyric-meta-title'); 
 			let output = label.find('pre').text();
-			console.log(output);
+			
 			$('.lyric-body').each((index, element) => {
 				const lyrics = $(element).text();
 				lyrics_list.push({
@@ -49,4 +49,5 @@ app.get('/api/captions', function(req, res) {
 	  })();
 	}
 });
+
 app.listen(3000);
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,25 +1,39 @@
+function displayRecord(record) {
+	const contentEle = document.getElementById("messages");
+	const div = document.createElement("div");
+	div.innerText = record["lyrics"];
+	contentEle.appendChild(div);
+
+}
+
 function displayMessages() {
-/*
-	const tbody = document.querySelector("tbody");
-    while (tbody.firstChild) {
-        tbody.removeChild(tbody.firstChild);
-    }
-*/
+
+
 	let query = "";
 	let keyword = document.getElementById("filterWord");
 	if (keyword.value != null || keyword.value != "") {
 		query = "type=" + keyword.value;
 	}
-	console.log (query);
+//	console.log (query);
 	let url = 'http://localhost:3000/api/captions';
 	if (query != "") {
 		url += "?" + query;
 	}
 
-	
+	const contentEle = document.getElementById("messages");
+    while (contentEle.firstChild) {
+        contentEle.removeChild(contentEle.firstChild);
+    }
 	fetch(url)
-  	.then(response => response.json());
-  //	.then(data => data.forEach(element => addReview(element)));
+  	.then(response => response.json())
+	.then(data => {
+		{
+			data.forEach(record => displayRecord(record))
+		}	
+	})
+	.catch((error) => {
+	  	console.error('Error:', error);
+	});
 }
 
 function clickFilter(evt) {
