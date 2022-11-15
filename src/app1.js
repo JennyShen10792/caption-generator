4da1d593119212b155c78bd94034acd91e7318ec
Niru Kumar <nirukumar@MacBook-Pro-261.lan>
2022-11-15 03:21:57 -0500

    faster movies, added puns and quotes

--- src/app1.js
+++ src/app1.js
@@ -40,6 +40,7 @@ app.get('/api/captions', function(req, res) {
    		  try {
 
 			let $ = cheerio.load(response);
+            
             let label = $('.lyric-meta-title'); 
             console.log(label);
 	        let output = label.find('pre').text();
@@ -62,16 +63,51 @@ app.get('/api/captions', function(req, res) {
 		}
 
 
-    
-        else if (req.query.category == "movies"){
-			let quotes_list = [];
+
+        if (req.query.category == "puns"){
+			let puns_list = [];
 		
 		(async () => {
 	
-		  const movieURL = ('https://www.quodb.com/search/' + req.query.type + '?advance-search=false&keywords=' + req.query.type);
-          console.log(movieURL);
+		  const punsURL = ('https://puns.samueltaylor.org/?word=' + req.query.type);
+          let response = await request(punsURL);
+
+          try {
+
+			let $ = cheerio.load(response);
+            
+            let label = $('.puns'); 
+	        let output = label.find('li').text();
+
+			$('#puns-list').each((index, element) => {
+				const captions = $(element).text();
+				puns_list.push({
+					'captions': captions
+				})
+			});
+
+     	 }  catch(err) {
+          		console.log(err);
+      	 }
+
+      
+      console.log(puns_list)
+      res.send(puns_list);	
+	  })();
+		}
+
+
+
+
+
+        else if (req.query.category == "people"){
+			let people_list = [];
+		
+		(async () => {
+	
+		  const peopleURL = ('https://www.quotes.net/quotations/' + req.query.type);
           const response = await request ({
-            uri: movieURL,
+            uri: peopleURL,
             headers: {
                 'Accept':
                 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -79,7 +115,7 @@ app.get('/api/captions', function(req, res) {
         'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
         'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive',
-        'Host': 'www.quodb.com',
+        'Host': 'www.quotes.net',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
             },
@@ -88,85 +124,57 @@ app.get('/api/captions', function(req, res) {
         });
 
    		  try {
+
 			let $ = cheerio.load(response);
-            let label = $('.movie_thumb'); 
-            let output = label.find('a').text();
+            let label = $('.tal qx'); 
+            console.log(label);
+	        let output = label.find('a').text();
+
+			$('.author-quote').each((index, element) => {
+				const captions = $(element).text();
+				people_list.push({
+					'captions': captions
+				})
+			});
+
+     	 }  catch(err) {
+          		console.log(err);
+      	 }
+
+      
+      console.log(people_list)
+      res.send(people_list);	
+	  })();
+		}
+
+
+    
+        else if (req.query.category == "movies"){
+			let quotes_list = [];
+		
+		(async () => {
+	
+		  let movieURL = ('https://www.moviequotes.com/search-quotes/?q=' + req.query.type);
+          let response = await request(movieURL);
+        
+
             
-            $('.phrase').each((index, element) => {
-                const captions = $(element).text();
-                quotes_list.push({
-                    'captions': captions
-                })
-            });
-
-
-
-
-
-            // let label = $('.phrase'); 
-            // console.log(label);
-            // let label2 = $("a.phrase > span");
-            // console.log(label2);
-            //let output = label.find('a');
-            //console.log(output);
-            //let quote_caption = output.find('span');
-            //console.log(quote_caption);
             
-            $(label2).each((index, element) => {
-                const captions = $(element).text();
-                console.log("hello");
-                quotes_list.push({
-                    'captions': captions
-                })
-            });
 
 
+   		  try {
 
+            let $ = cheerio.load(response);
+            let label = $('[class=\"post-box\"]');
+            let output = label.find('span').text();
+            
 
-            // let label = $('.phrase'); 
-            // let label2 = $("a.phrase > span").text();
-            // console.log(label2);
-            // let label1 = label.text();
-            // console.log(label);
-            // let output = label.find('span').text();
-            //console.log(output);
-
-
-			// $('.phrase').each((index, element) => {
-            //     const captions = $(element).text();
-            //     quotes_list.push({
-            //         'captions': captions
-            //     })
-            // });
-
-            // let $ = cheerio.load(response);
-            // let captions = $('a[class="phrase"] > span').text();
-            // console.log(captions);
-            // 	quotes_list.push({
-			// 		'captions': captions
-			// 	})
-
-            // let label = $('.table result_table'); 
-            // let block = label.find('blockquote');
-            // let quote = block.find('')
-            // let output = label.find('').text();
-            // console.log(output);
-            // $('.phrase').each((index, element) => {
-            //     const captions = $(element).text();
-            //     console.log(captions);
-            //     quotes_list.push({
-            //         'captions': captions
-            //     })
-            // });
-
-
-			// $('.lyric-body').each((index, element) => {
-			// 	const captions = $(element).text();
-			// 	lyrics_list.push({
-			// 		'captions': captions
-			// 	})
-			// });
-
+			$('[class=\"whole-read-more\"]').each((index, element) => {
+				const captions = $(element).text();
+				quotes_list.push({
+					'captions': captions
+				})
+			});
 
 
 
