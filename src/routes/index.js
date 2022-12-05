892f2f8d8f0c1ce79257f0bee59804db47e39252
Jeffrey Saeteros <js11692@nyu.edu>
2022-12-05 12:59:35 -0500

    after reg goto login & validating login + register

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,18 @@ const express = require('express'),
   passport = require('passport'),
   mongoose = require('mongoose'),
   User = mongoose.model('User'),
-Caption = mongoose.model('Caption');
+  Caption = mongoose.model('Caption');
+
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/login');
+}
+
+function isLoggedOut(req, res, next) {
+  if (!req.isAuthenticated()) return next();
+  res.redirect('/');
+}
 
 router.get('/logout', function (req, res) {
   req.logout(function (err) {
@@ -16,57 +27,76 @@ router.get('/logout', function (req, res) {
 router.get('/', (req, res) => {
   res.render('home');
 });
+// router.get('/', isLoggedIn, (req, res) => {
+//   res.render('home');
+// });
 
-router.get('/login', (req, res) => {
-  res.render('login');
+// router.get('/login', (req, res) => {
+//   res.render('login');
+// });
+
+router.get('/login', isLoggedOut, (req, res) => {
+  const response = {
+    title: "Login",
+    error: req.query.error
+  }
+
+  res.render('login', response);
 });
 
-router.get('/register', (req, res) => {
-  res.render('register');
+router.get('/register', isLoggedOut, (req, res) => {
+  const response = {
+    title: "Register",
+    error: req.query.error
+  }
+
+  res.render('register', response);
 });
+
 /*
 router.get('/captions', (req, res) => {
-  	User.find(function(err, captions, count) {
-		res.render( 'captions', {
-			captions: captions
-		});
-	});
+    User.find(function(err, captions, count) {
+    res.render( 'captions', {
+      captions: captions
+    });
+  });
 })
 */
+
 router.get('/captions', (req, res) => {
-	Caption.find({user: req.user ? req.user._id : undefined}, (err, captions, count) => {
-		res.render('captions', {captions:captions});
-	});
+  Caption.find({ user: req.user ? req.user._id : undefined }, (err, captions, count) => {
+    res.render('captions', { captions: captions });
+  });
 });
 
 
 
 
-router.get('/captions/save', function(req, res) {
+router.get('/captions/save', function (req, res) {
   res.render('save');
 });
 
 
 
-router.post('/captions/save', function(req, res) {
-	console.log(req.body.captionName);
-	new Caption({
-		user:User,
-		name: req.body.captionName
-	}).save(function(err, caption, count){
-		res.redirect('/captions');
-	});
+router.post('/captions/save', function (req, res) {
+  console.log(req.body.captionName);
+  new Caption({
+    user: User,
+    name: req.body.captionName
+  }).save(function (err, caption, count) {
+    res.redirect('/captions');
+  });
 });
 
 router.get('/captions/delete', (req, res) => {
-	Caption.findByIdAndRemove(req.query.id, function (err) {
-  		if (err) {
-  			console.log(err);
-		}
-		else {
-			res.redirect('/captions');
-		}
-	});			
+  Caption.findByIdAndRemove(req.query.id, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.redirect('/captions');
+    }
+  });
 });
 
 
@@ -78,11 +108,14 @@ router.post('/register', (req, res) => {
   const { username, password } = req.body;
   User.register(new User({ username }), req.body.password, (err, user) => {
     if (err) {
-      res.render('register', { message: 'Your registration information is not valid' });
+      // res.render('register', { message: 'Your registration information is not valid' });
+      res.redirect('/register?error=true')
     } else {
-      passport.authenticate('local')(req, res, function () {
-        res.redirect('/');
-      });
+      res.redirect('/login');
+      // passport.authenticate('local')(req, res, function () {
+      //   // res.redirect('/');
+      //   res.redirect('/login');
+      // });
     }
   });
 });
@@ -94,11 +127,13 @@ router.post('/login', (req, res, next) => {
         res.redirect('/');
       });
     } else {
-      res.render('login', { message: 'Your login or password is incorrect.' });
+      // res.render('login', { message: 'Your login or password is incorrect.' });
+      res.redirect('/login?error=true');
     }
   })(req, res, next);
 });
+
 router.post('/captions', (req, res) => {
-	res.render('captions')
+  res.render('captions')
 });
 module.exports = router;
